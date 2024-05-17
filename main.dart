149b8d16cfd8db7f446cabfc151373599f795cf4
Can Çokçapar <103719457+mahmutcancokcapar@users.noreply.github.com>
2024-05-17 04:43:35 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Stepper Widget',
       home: AnaSayfa(),
     );
   }
